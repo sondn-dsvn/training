73a7289d3013c15d81e753518eeb210dc0e99ea8
sondn-dsvn <son-duong@dimage.co.jp>
2020-08-14 16:42:40 +0700

    Init base class.
    Signed-off-by: sondn-dsvn <son-duong@dimage.co.jp>
    
    Base classes are parent class and when making a class you must extend
    this base class.
    Move user model to Models folder.

---
+++ app/Http/Requests/Api/BaseRequest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BaseRequest extends FormRequest
+{
+}
---
+++ app/Models/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+
+class User extends BaseModel implements
+    AuthenticatableContract,
+    AuthorizableContract
+{
+    use Authenticatable, Authorizable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
